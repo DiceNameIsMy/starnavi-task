38af3ef3c37bf0eeebfc535c46238cb9fd6686b4
Dice <dicenameismy@gmail.com>
2021-08-11 16:39:46 +0600

    configure urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include([
+
+        path('v1/', include([
+            path('accounts/', include('apps.accounts.api.v1.urls')),
+            path('posts/', include('apps.posts.api.v1.urls')),
+        ]))
+
+    ]))
 ]
