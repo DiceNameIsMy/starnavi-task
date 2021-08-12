690eb85836276e695fb1f9339873d12b512568e4
Dice <dicenameismy@gmail.com>
2021-08-12 09:28:35 +0600

    simplify PostStatsView

--- apps/posts/api/v1/views.py
+++ apps/posts/api/v1/views.py
@@ -6,8 +6,6 @@ from rest_framework.status import HTTP_201_CREATED ,HTTP_204_NO_CONTENT
 from rest_framework.filters import OrderingFilter
 
 from django.shortcuts import get_object_or_404
-from django.core.exceptions import ValidationError
-from django.utils.timezone import datetime, make_aware
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -40,23 +38,17 @@ class RetrieveUpdateDestroyPostView(RetrieveUpdateDestroyAPIView):
 class PostStatsView(GenericAPIView):
     permission_classes = (IsAuthenticated, IsAuthor)
 
-    queryset = Post.objects.all()
+    filter_backends = [DateFilter]
+    date_fields = ['date']
 
     def get(self, request, pk):
-        start_date = request.GET.get('start_date')
-        end_date = request.GET.get('end_date', str(datetime.today().date()))
+        post: Post = get_object_or_404(Post, pk=pk)
+        self.check_object_permissions(self.request, post)
 
-        if not start_date:
-            raise ValidationError("start_date should be provided")
-
-        # TODO better naming
-        start_date_date = make_aware(datetime.strptime(start_date, "%Y-%m-%d"))
-        end_date_date = make_aware(datetime.strptime(end_date, "%Y-%m-%d"))
-
-        post: Post = self.get_object()
         post_likes = Like.objects.filter(post=post)
+        post_likes = self.filter_queryset(post_likes)
 
-        stats = get_likes_stats(post_likes, start_date_date, end_date_date)
+        stats = get_likes_stats(post_likes)
 
         return Response(stats)
 
