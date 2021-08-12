e63844075f6bfdfa1b5325b88eaaee2f438a4d17
Dice <dicenameismy@gmail.com>
2021-08-12 09:27:03 +0600

    simplify stats service

--- apps/posts/api/v1/services.py
+++ apps/posts/api/v1/services.py
@@ -5,17 +5,10 @@ from django.core.exceptions import ValidationError
 from ...models import Post, Like
 
 
-def get_likes_stats(post_likes, start_date: date, end_date: date) -> dict:
-    if start_date > end_date:
-        raise ValidationError("start_date shouldn't be bigger than the end_date")
+def get_likes_stats(post_likes) -> dict:
     results = {}
 
-    sliced_likes = post_likes.filter(
-        date__date__gte=start_date, 
-        date__date__lte=end_date
-    )
-
-    for like in sliced_likes:
+    for like in post_likes:
         date = str(like.date.date())
         if date in results:
             results[date] += 1
